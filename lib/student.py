52757cfcd2c3ed4253ed1d9ed850523440da1aff
Kelvin Gil <gilkelvin2@gmail.com>
2024-06-14 14:49:46 -0400

    done

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,6 @@ from user import User
 
 class Student(User):
     
-    def learn(self):
-        pass
+    def learn(self,knowledge):
+        super().__init__(self.first_name,self.last_name)
+        self.knowledge.append(knowledge)
